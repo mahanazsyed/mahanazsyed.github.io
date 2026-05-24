28170c10c7bae495c30d938ea77e383821c3d863
mahanazsyed <mahanazuddinsyed@gmail.com>
2026-05-24 14:24:17 +0000

    Deploying to gh-pages from @ mahanazsyed/mahanazsyed.github.io@7ace250356df0f35b5cfe3e68475b7be099347dd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -469,14 +469,14 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/Mahanaz_Syed_CV.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%73%79%65%64%6D%31@%75%74%68%73%63%73%61.%65%64%75", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -497,14 +497,14 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=7lU_LvYAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://mahanazsyed.github.io/", "_blank");
         },
       },{
       id: 'light-theme',
